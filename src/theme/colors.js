0227f9a49e30924f2d3f2f8a73e1c34ee91fdb38
buumu8 <buumu8@gmail.com>
2022-04-29 13:00:41 +0700

    add theme colors and netlify functions setup

---
+++ src/theme/colors.js
@@ -0,0 +1,12 @@
+export const colors = {
+  brand: {
+    primary: "#ff3482",
+  },
+  background: {
+    primary: "#45badd",
+    secondary: "#ffffff",
+  },
+  text: {
+    primary: "#000000",
+  },
+};
